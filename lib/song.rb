b57c36cacf9232b1ad3517068bf9d30e8eaf851a
Marcus Jun <mjun@calstate.edu>
2018-04-15 04:25:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,11 +54,12 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name)
-      self.find_by_name(name)
-    else
-      self.create_by_name(name)
-    end
+    (self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name))
+    #if self.find_by_name(name)
+      #self.find_by_name(name)
+    #else
+      #self.create_by_name(name)
+    #end
 
     #if self.all.include? {|music| music.name==name}
       #self.find_by_name(name)
